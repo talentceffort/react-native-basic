db237de0333fa2d849f3275629fe99859cba6a75
talentceffort <talentceffort@gmail.com>
2020-06-09 21:28:49 +0900

    init Setting

--- src/Component/App.tsx
+++ src/Component/App.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const add = 3;
+
+  const obj = {
+    a: 3,
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
