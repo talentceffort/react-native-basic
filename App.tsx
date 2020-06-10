b011cc6d4d071cacad891f16b6ecdf724bb2c6e4
talentceffort <talentceffort@gmail.com>
2020-06-10 22:14:00 +0900

    Init Config

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
-import * as React from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { ApolloProvider } from 'react-apollo-hooks';
+import ApolloClient from 'apollo-boost';
+import apolloClientOptions from './apollo';
+
+const client = new ApolloClient({
+  ...apolloClientOptions,
+});
 
 function HomeScreen() {
   return (
@@ -15,11 +22,14 @@ const Stack = createStackNavigator();
 
 function App() {
   return (
+  <ApolloProvider client = {client}>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name='Home' component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
+  </ApolloProvider>
+
   );
 }
 
